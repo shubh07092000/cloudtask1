a677be104f2b3eb5cecea085f502f71c9d1fec81
shubh gupta <guptashubh1717@gmail.com>
2020-06-12 18:14:30 +0530

    final1

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-This is Shubh Gupta's page
-<br />
+<b> This is Shubh Gupta's page</b> 
 <pre>
 <?php
-
+echo "<body style='background-color:aqua'>";
 echo "<img src='dzysjyuepsgzu.cloudfront.net/shubh.jpeg' width='1000' height='800'/>";
 
 ?>
